dbe7b299174e70fa5002970d04b81521852762fe
avivSolomon <saviv149@gmail.com>
2023-06-27 15:54:34 +0300

    start point again

--- muscle.py
+++ muscle.py
@@ -12,3 +12,81 @@ class Muscle():
         self.date = date
         self.rast_time = rast_time
 
+    # class cardiopulmonary_endurance(Muscle): # סיבולת לב-ריאה
+    def running(self):
+        pass
+    def cycling(self):
+        pass
+
+    # class chest(Muscle):#  חזה
+    def bench_press(self):
+        pass
+    def push_ups(self):
+        pass
+    def chest_flyes(self):
+        pass
+    def dumbbell_press(self):
+        pass
+
+    # class back(Muscle): #גב
+    def pull_ups(self):
+        pass
+    def rows(self):
+        pass
+    def lat_pulldowns(self):
+        pass
+
+    # class Shoulders(Muscle):  # כתפיים
+    def shoulder_press(self):
+        pass
+    def lateral_raises(self):
+        pass
+    def front_raises(self):
+        pass
+
+    # class Biceps(Muscle): # יד קדמית
+    def bicep_curls(self):
+        pass
+    def hammer_curls(self):
+        pass
+
+    # class Triceps(Muscle):  #  יד אחורית
+    def Tricep_dips(self):
+        pass
+    def tricep_pushdowns(self):
+        pass
+
+    # class Quadriceps(Muscle):  #  4 ראשי
+    def squats(self):
+        pass
+    def lunges(self):
+        pass
+    def leg_press(self):
+        pass
+
+    # class Hamstrings(Muscle):  # ירכיים
+    def deadlifts(self):
+        pass
+    def hamstring_curls(self):
+        pass
+
+    # class Calves(Muscle): # תאומים
+    def Calf_raises(self):
+        pass
+
+    # class Abdominals(Muscle):# בטן
+    def planks(self):
+        pass
+    def sit_ups(self):
+        pass
+    def Russian_twists(self):
+        pass
+
+    def getvalue(self):
+        return [self.points, self.date, self.rast_time]
+
+# Retrieve all classes that inherit from BaseClass
+subclasses = Muscle.__subclasses__()
+# Create instances of each subclass
+# muscle_subclasses = [subclass() for subclass in subclasses]
+# print(muscle_subclasses)
--- userData.py
+++ userData.py
@@ -1,4 +1,4 @@
-import muscle
+import muscle, exercises
 import pandas as pd
 
 # muscle_list = ['user_name', 'cardiopulmonary_endurance', 'chest', 'back',
@@ -7,9 +7,9 @@ import pandas as pd
 #
 # new_list = ['user_name']
 # for elem in muscle_list[1:]:
-#     new_list.append(elem + '_point')
-#     new_list.append(elem + '_date')
-#     new_list.append(elem + '_rest')
+#     new_list.append(elem + '__point')
+#     new_list.append(elem + '__date')
+#     new_list.append(elem + '__rest')
 #
 # df = pd.DataFrame(columns=new_list)
 # df.to_csv('data.csv')
@@ -19,28 +19,37 @@ class user_data():
                    'shoulders', 'biceps', 'triceps', 'quadriceps',
                    'hamstrings', 'calves', 'abdominal']
 
-    def __init__(self, username):
+    def __init__(self, username, weight=70, height=1.75):
         self.user_name = username
         self.values = self.get_data(username)
+        self.height = height
+        self.weight = weight
 
     def get_data(self, username, patch='data.csv'):
         data = pd.read_csv(patch)
         user_set = data['user_name']
         if username in user_set:
-            return dict(data[username])
+            user_val = data.loc[data.user_name == username].tolist()[1:]
+            return {user_data.muscle_list[i]: muscle.Muscle(tuple(user_val[i*3:(i+1)*3])) \
+                    for i in range(len(user_data.muscle_list))}
         else:
             return {k: muscle.Muscle() for k in user_data.muscle_list}
 
     def save_data(self, patch='data.csv'):
         df = pd.read_csv(patch)
-        df.loc[df['user_name'] == self.user_name] = self.values
+        val_list = [self.user_name]
+        for key in user_data.muscle_list:
+            val_list.append(self.values[key].getvalue())
+        if self.user_name in df['user_name'].tolist():
+            df.loc[df['user_name'] == self.user_name] = val_list
+        else:
+            df.iloc[len(df.index)] = val_list
         df.to_csv('data.csv')
 
-
-    def __str__(self):
-
-        return ''.join(self.user_name) + "\n" + ' ;'.join(self.values)
+    def print_val(self):
+        for key in user_data.muscle_list:
+            print(key, self.values[key].getvalue())
 
 
 ari = user_data('ari')
-print(ari)
+ari.save_data()
