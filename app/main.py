3aadf71f597482176c7597142baa4f9a613702a1
Ari Yacobson <ariyacovson@gmail.com>
2023-06-29 16:36:56 +0300

    new structure

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from user_controller import UserController
+from exercise_controller import ExerciseController
+from muscle_controller import MuscleController
+
+if __name__ == "__main__":
+    user_controller = UserController()
+    exercise_controller = ExerciseController()
+    muscle_controller = MuscleController()
+
+    # Handle user interactions and orchestrate the program flow
+
+
+# Additional code for the entry point of the application
